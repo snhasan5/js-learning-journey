c5d2773f4511dcbed8a188a8e39bf9d3dd55db6d
snhasan5 <163376721+snhasan5@users.noreply.github.com>
2026-02-19 22:18:27 +0530

    looked into Objects

---
+++ Javascript/Objects/part2.js
@@ -0,0 +1,32 @@
+//singleton -> not literal made via constructor
+
+//object literals
+mySym = Symbol("Key2");
+const JsUser = {
+    name : "Hasan",
+    "age" : 18,
+    [mySym] : mySym, //if no square braces then string
+    location : "AP",
+    isLoggedIn : false,
+    lastLoginUser: ["Monday","Saturday"],
+    greeting: function greeting(args){
+        console.log(`hello ${this.name}`);
+        }
+}
+//accessing objects
+console.log(JsUser.name);
+console.log(JsUser['age']);
+console.log(typeof JsUser[mySym]);
+
+JsUser.email = 'nazeer@vitapstudent.ac.in'; //new property added
+// Object.freeze(JsUser); to freeze the values of the object
+JsUser.email = 'nazeer@vitapsnt.ac.in'; // updates the value
+
+//functions inside objects
+JsUser.greeting('hasan');
+JsUser.cuss = function(){
+    console.log("FuckOff");
+}
+console.log(JsUser);
+
+
